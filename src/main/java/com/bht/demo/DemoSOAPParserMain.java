cf296a6ab5021f4cb315ff251ca61454251c2139
binh.huynh1 <binh.huynh1@mservice.com.vn>
2021-04-06 00:54:43 +0700

    pre analyze

--- src/main/java/com/bht/demo/DemoSOAPParserMain.java
+++ src/main/java/com/bht/demo/DemoSOAPParserMain.java
@@ -1,9 +1,7 @@
 package com.bht.demo;
 
 import com.bht.demo.parser.SOAPParser;
-import org.bamboo.model.flightport.SeatMapDetailsType;
-import org.bamboo.model.flightport.SeatMapInformation;
-import org.bamboo.model.flightport.ShowSeatMapRS;
+import org.bamboo.model.flightport.*;
 
 import javax.xml.bind.JAXBException;
 import java.io.BufferedReader;
@@ -11,6 +9,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -20,7 +19,16 @@ import java.util.stream.Collectors;
  */
 public class DemoSOAPParserMain {
 
+    /**
+     * SeatMapConfiguration: X - Y - X ('-' stands for the aisle)
+     * For Example:
+     * BOEING A320
+     *      - BUSINESS: 2-0-2 (DEC: A - C-D - F)
+     *      - ECONOMY:  3-0-3 (DEC: A-B-C-D-E-F)
+     * BOEING A787
+     */
     public static void main(String[] args) throws JAXBException {
+        List<Character> DECAlphabets = Arrays.asList('A','B','C','D','E','F','H','J','K','L','M','N','P','R','S','T','U','V');
         InputStream inputStream = DemoSOAPParserMain.class.getClassLoader()
                 .getResourceAsStream("bamboo_seat_res.xml");
         String xmlString = new BufferedReader(new InputStreamReader(Objects
@@ -29,13 +37,22 @@ public class DemoSOAPParserMain {
                 .replaceAll("\\s*[\\r\\n]+\\s*", "");
 
         ShowSeatMapRS showSeatMapRS = SOAPParser.toObject(xmlString, ShowSeatMapRS.class);
-        SeatMapInformation seatMapInformation = showSeatMapRS.getSeatMapInformation();
+        SeatMapInformation seatMapInformation = Objects.requireNonNull(showSeatMapRS).getSeatMapInformation();
         List<String> attachedSsr = seatMapInformation.getAttachedSsr();
         List<String> zoneAttributes = seatMapInformation.getZoneAttributes();
+
+        /**
+         *  'W': window seat
+         *  'A': aisle seat
+         */
         List<String> locationAttribute = seatMapInformation.getLocationAttribute();
         List<String> seatPriorityAttributes = seatMapInformation.getSeatPriorityAttributes();
         BigInteger maxInfantsPerUnit = seatMapInformation.getMaximumInfantsPerUnit();
         SeatMapDetailsType seatMapdetails = seatMapInformation.getSeatMapdetails();
-        System.out.println(showSeatMapRS);
+        DeckDetailsType deckDetailsType = seatMapdetails.getDeckDetails().get(0);
+        Integer totalSeats = deckDetailsType.getTotalSeats();
+        List<SeatAttributesPositionType> seatAttributesPositionTypes = deckDetailsType.getSeatAttributesPosition();
+        List<CabinDetailsType> cabinDetailsTypes = deckDetailsType.getCabinDetails();
+        System.out.println(cabinDetailsTypes);
     }
 }
