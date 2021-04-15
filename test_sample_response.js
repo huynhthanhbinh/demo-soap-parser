db2cf941933a1b8f53e62cb2efa540939a350d83
binh.huynh1 <binh.huynh1@mservice.com.vn>
2021-04-15 22:50:15 +0700

    test sample response with js by using Node

---
+++ test_sample_response.js
@@ -0,0 +1,8 @@
+/**
+ * Run this code with Node to test the sample response json
+ * Command: node <file_name>.js
+ * Require: install Node before
+ */
+
+let sampleResponseJO = require("./sample_response_v2.json");
+console.log(sampleResponseJO);
